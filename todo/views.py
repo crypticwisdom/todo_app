890e8cf85805857cce9b52f37cdd0718c8132c8c
wisdom <crypticwisdom84@gmail.com>
2021-09-13 19:57:19 +0100

    add the bootstrap link

--- todo/views.py
+++ todo/views.py
@@ -22,10 +22,8 @@ def index(request):
     obj_list = None
     
     user = request.user
-    # l = user.task_set.all()
     task_form = TaskForm()
     if request.method == 'POST':
-        # print(request.POST)
         try:
 
             title = request.POST['text']
@@ -33,24 +31,19 @@ def index(request):
             email = request.POST['email']
             print(date)
             task_obj = Task(title=title, when_to_do=date, email=email)
-            for t in Task.objects.all():
-                if title == t.title and (email == t.email or t.email == '' or t.email == None):
+            for task in Task.objects.all():
+                if title == task.title and (email == task.email or task.email == '' or task.email == None):
                     return redirect(reverse('todo:index'))
             task_obj.save()
         except:
             messages.error(request, f"There was error in submitting your task, enter date field.")
             return redirect(reverse('todo:index'))
         else:
-
-        
         # check if this data already exist for this email
-            
-
+ 
             tasks = Task.objects.all().filter(email=email)[::-1]
             obj_list = tasks[:5]
-        # if request.POST['logged_create']:
-    #     print('sdsd')
-    #     print(request.user.is_authenticated)
+
     return render(request, 'todo/index.html', {
         'tasks':obj_list,
         'task_form':task_form,
@@ -61,7 +54,6 @@ def login_page(request):
         return redirect(reverse('todo:index'))
  
     login_form = LoginForm()
-    # print(LoginForm(request.POST), request.POST)
     if request.method == 'POST':
         try:
             login_form = RegistrationForm(request.POST)
@@ -70,7 +62,7 @@ def login_page(request):
             password = request.POST['password']
             
             user = authenticate(request, username=username, password=password)
-            # print(user.username, user.email, user.password)
+            
             print(user.id)
             if user is not None:
                 login(request, user)
@@ -80,25 +72,18 @@ def login_page(request):
             messages.info(request, f"Please fill the form")
             return HttpResponseRedirect(reverse('todo:login_page'))
         else:
-                # print(request.user)
-
+        
             #get user from User model 
             user_obj = User.objects.get(username=user.id)
-            # print(user_obj.email)
-
 
             all_task = Task.objects.all()
             for task in all_task:
                 if task.user_id == None and task.email == user_obj.email:
-                    # print(task.id, task.user_id, user_obj.id)
                     
                     task_object = Task.objects.get(pk=task.id)
-                    # print(task_object)
                     task_object.user_id = user_obj.id
                     task_object.save()
             return HttpResponseRedirect(reverse('todo:user_page', args=(user_obj.id,)))
-
-            # return redirect(reverse('todo:index'))
 
     return render(request, 'todo/login.html', {
         'login_form':login_form,
@@ -119,10 +104,6 @@ def register_page(request):
             username = register_form.cleaned_data['username']
 
             email = register_form.cleaned_data['email']
-
-            # print(register_form.cleaned_data)
-
-            # print(User.objects.get(username=username, email=email))
         # Get user records with the register email and username
         # fetch all task, loop through task to find if this user has a user_id == None with this email.
         # get task user_id and bind with this reg. user id
@@ -143,13 +124,6 @@ def register_page(request):
             messages.success(request, f"Successfully created {username}, Enter credentals to Login.")    
             return redirect(reverse('todo:login_page'))
                     
-
-            # return redirect(reverse('todo:login_page'))
-                
-                
-            # print(username, request.user.id)
-            
-            # return redirect(reverse('todo:login_page'))
     return render(request, 'todo/register.html', {
 
         'register_form':register_form,
@@ -163,7 +137,7 @@ def user_page(request, pk):
     user_obj = get_object_or_404(User, pk=pk)
 
     if request.method == "POST":
-        # print(request.POST, request.POST['text'])
+
         try:
             title = request.POST['text']
             date = request.POST['date']
